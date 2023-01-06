854d1121af9e10a85aa34dbb188a3935b81b97c0
TJ Lambert <tjlambert@microsoft.com>
2023-01-06 17:10:02 -0600

    Create the sample

---
+++ EditorTest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EditorTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
